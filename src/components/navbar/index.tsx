d83b72e9674b15b63005056d7ad6bda33de3b7d5
JusAeng <justo_play@hotmail.com>
2022-11-03 23:34:35 +0700

    nav dropdown

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,48 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'components/navbar/index.scss';
 import logo from 'assets/images/logo.png';
 import { FiMenu } from 'react-icons/fi';
 import { MdClose } from 'react-icons/md';
-import { Link, useNavigate } from 'react-router-dom';
-
-const NavMenu = () => {
-  return (
-    <div className='nav-dropdown'>
-      <Link to='/' className='nav-link'>
-        <div className='nav-menu'>Home Page</div>
-      </Link>
-      <Link to='/find-notes' className='nav-link'>
-        <div className='nav-menu'>Find Notes</div>
-      </Link>
-      <Link to='/share-notes' className='nav-link'>
-        <div className='nav-menu'>Share Notes</div>
-      </Link>
-    </div>
-  );
-};
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  const [clickMenu, setClike] = useState<Boolean>(true);
+  const [openNav, setOpenNav] = useState<Boolean>(false);
 
   const handleClick = () => {
-    setClike(!clickMenu);
-    // console.log(clickMenu);
+    setOpenNav(!openNav);
   };
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    let handler = (e: MouseEvent) => {
+      if (e.target) {
+        setOpenNav(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    console.log('ineffect');
+  }, []);
+
   return (
     <div>
       <div className='nav-container'>
-        {clickMenu ? (
-          <FiMenu size={30} className='nav-element' onClick={handleClick} />
-        ) : (
+        {/* tringer dropdown */}
+        {openNav ? (
           <MdClose size={30} onClick={handleClick} className='nav-element' />
+        ) : (
+          <FiMenu size={30} className='nav-element' onClick={handleClick} />
         )}
+
         <img src={logo} className='nav-logo' />
         <img src={logo} className='nav-profile' onClick={() => navigate('/profile')} />
       </div>
-      {!clickMenu && <NavMenu />}
+
+      {/* NavMenu */}
+      <div className={`nav-dropdown ${openNav ? 'active' : 'inactive'}`}>
+        <div className='nav-menu' onClick={() => navigate('/')}>
+          Home Page
+        </div>
+        <div className='nav-menu' onClick={() => navigate('/find-notes')}>
+          Find Notes
+        </div>
+        <div className='nav-menu' onClick={() => navigate('/share-notes')}>
+          Share Notes
+        </div>
+      </div>
     </div>
   );
 };
