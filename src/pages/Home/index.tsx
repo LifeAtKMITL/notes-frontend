185f5388b8eaf97abc2f05651b88ae5356ef6b08
JusAeng <justo_play@hotmail.com>
2022-11-05 17:55:26 +0700

    test component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
+import DropdownSelect from 'components/dropdown-select';
 import React from 'react';
+import './index.scss';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <DropdownSelect lst={['nani', 'anya', 'Yor', 'Loid', 'bond']} className='to-ddsl' />
+    </div>
+  );
 };
 
 export default HomePage;
