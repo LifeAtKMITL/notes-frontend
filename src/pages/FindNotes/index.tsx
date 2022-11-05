f253bddb358c533e32d65e12f67d79484a9c6f14
JusAeng <justo_play@hotmail.com>
2022-11-05 17:55:09 +0700

    change class name

--- src/pages/FindNotes/index.tsx
+++ src/pages/FindNotes/index.tsx
@@ -37,7 +37,7 @@ const FindNotesPage = () => {
       <div className='search'>
         <div className='search-box'>
           <input type='text' />
-          <button>
+          <button className='button-search'>
             <IoSearchOutline size={15} />
           </button>
         </div>
