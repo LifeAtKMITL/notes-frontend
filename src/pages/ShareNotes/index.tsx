40eb85a0bb2eec012b73254e083ca7042a68d703
JusAeng <justo_play@hotmail.com>
2022-11-10 01:10:04 +0700

    add subjectId option select

--- src/pages/ShareNotes/index.tsx
+++ src/pages/ShareNotes/index.tsx
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'pages/ShareNotes/index.scss';
 import DropdownSelect from 'components/dropdown-select';
+import axios from 'axios';
+
+interface ISubject {
+  subjectId: string;
+  name: string;
+}
+interface Iprop {
+  subjects: Array<ISubject>;
+}
 
 const ShareNotesPage = () => {
   const handleSubmit = () => {
@@ -10,9 +19,56 @@ const ShareNotesPage = () => {
     return;
   };
 
+  //Data to fill subjectID =>  subjectName
+  const [showSubjects, setShowSubjects] = useState<boolean>(false);
+  const [tosentSubjectId, setTosentSubjectId] = useState<string>('');
+  const [tosentSubjectName, setTosentSubjectName] = useState<string>('');
+
+  const [subjects, setSubjects] = useState<ISubject[]>([]);
+  const loadSubjects = async () => {
+    const res = await axios.get('https://life-at-kmitl-backend-production.up.railway.app/subject');
+    setSubjects(res.data);
+  };
+
+  const selectSubject = (id: string, name: string) => {
+    setTosentSubjectId(id);
+    setTosentSubjectName(name);
+    setShowSubjects(false);
+  };
+
+  const handleInputChange = (e: React.FormEvent<HTMLElement>) => {
+    setShowSubjects(true);
+  };
+
+  const SubjectMenu: React.FC = () => {
+    useEffect(() => {
+      let handler = () => {
+        setShowSubjects(false);
+      };
+
+      document.addEventListener('mousedown', handler);
+    });
+
+    return (
+      <div className='box'>
+        {subjects.map((subject) => {
+          if (subject.subjectId[0] == '9') {
+            return (
+              <div
+                className='subject-menu'
+                key={subjects.indexOf(subject)}
+                onClick={() => selectSubject(subject.subjectId, subject.name)}
+              >{`${subject.subjectId}${subject.name}`}</div>
+            );
+          }
+        })}
+      </div>
+    );
+  };
+
+  //Data from DDSL =========
   const [tosentExam, setTosentExam] = useState<string>('Exam');
   const [tosentYear, setTosentYear] = useState<string>('Year');
-
   const updateState = (state: string): void => {
     if (!isNaN(Number(state))) {
       setTosentYear(state);
@@ -25,17 +81,22 @@ const ShareNotesPage = () => {
     }
   };
 
+  useEffect(() => {
+    loadSubjects();
+  }, []);
+
   return (
-    <div>
-      <form className='form-sharing' onSubmit={handleSubmit}>
+    <div className='sharenote-page'>
+      <form className='form' onSubmit={handleSubmit}>
         <div className='title'>
           <h2>New Note</h2>
         </div>
-        <div className='input-contain'>
+        <div className='contain1'>
           <p>Subject ID</p>
-          <input type='text' />
+          <input type='text' name='subjectId' onChange={handleInputChange} />
+          {showSubjects && <SubjectMenu />}
         </div>
-        <div className='input-contain'>
+        <div className='contain1'>
           <p>Teacher Name</p>
           <input type='text' />
         </div>
@@ -58,7 +119,7 @@ const ShareNotesPage = () => {
             />
           </div>
         </div>
-        <div className='input-contain'>
+        <div className='contain1'>
           <p>Description</p>
           <textarea></textarea>
         </div>
@@ -72,6 +133,13 @@ const ShareNotesPage = () => {
           </button>
         </div>
       </form>
+      <button
+        onClick={() => {
+          console.log(tosentSubjectId, tosentSubjectName);
+        }}
+      >
+        Click
+      </button>
     </div>
   );
 };
