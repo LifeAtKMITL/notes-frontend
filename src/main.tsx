fbf7a8f119658e83f5a7bc91e37222316e5cb6f1
zantaclaus <tarzuto@gmail.com>
2022-11-14 18:01:00 +0700

    Implement AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import AuthProvider from 'contexts/auth/AuthProvider';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,7 +8,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
