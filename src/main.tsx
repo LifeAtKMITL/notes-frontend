0324e775952125500adfccdcbf34117da0e8a3c3
zantaclaus <tarzuto@gmail.com>
2022-11-16 13:47:59 +0700

    🛠 Update LIff-id

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import AuthProvider from 'contexts/auth/AuthProvider';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { LiffProvider } from 'react-liff';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
@@ -8,9 +9,11 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <LiffProvider liffId='1657631189-Q7yGOJmZ'>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </LiffProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
